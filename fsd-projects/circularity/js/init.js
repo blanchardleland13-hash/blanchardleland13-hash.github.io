d1018c6db40c1426185edeb5f00e5ead2964fc0a
blanchardleland13-hash <blanchardleland13@gmail.com>
2026-09-25 12:45:23 +0000

    message

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -1,3 +1,5 @@
+const Gamification = require("./gamification");
+
 var init = function (window) {
     'use strict';
     var 
@@ -20,19 +22,35 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+      var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            var circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+      }
+
+        Gamification.init({
+            canvas: canvas,
+            view: view,
+            draw: draw,
+            physikz: physikz,
+            circles: circles,
+            game: game
+        });
+
 
 
         // TODO 3 : Call the drawCircle() function
-
-
+  
 
         // TODO 7 : Use a loop to create multiple circles
-
+        for (var i = 0; i < 55; i++){
+            drawCircle();
+        }
 
 
 
@@ -47,16 +65,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
-            
+        
             // TODO 5 : Call game.checkCirclePosition() on your circles
            
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+           for(var i = 0; i < circles.length; i++){
+            physikz.updatePosition(circles[i]);
+            game.checkCirclePosition(circles[i]);
+           }
+
+           Gamification.update();
         }
-    
+     
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -70,9 +91,17 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+           if (circle.x < 0){
+            circle.x = canvas.width
+           } 
+         
 
-
+         if (circle.y < 0){
+            circle.y = canvas.height
+         }
+         if (circle.y > canvas.height){
+            circle.y = 0;
+         }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
